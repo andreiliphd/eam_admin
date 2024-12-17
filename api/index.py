c6aaf78a1ad6d182ef0b4bb1d422e8a7b81e5e4d
Andrei Li <andreiliphd@gmail.com>
2024-12-17 08:30:20 +0300

    NORE

--- api/index.py
+++ api/index.py
@@ -19,6 +19,7 @@ redis = Redis(url=os.getenv("HOST_REDIS"), token=os.getenv("PASSWORD_REDIS"))
 def telegram_url_builder(method, payload):
     basic = "https://api.telegram.org/bot" + "7881036983:AAGHguPzNqEh3StCC8l1iTsXzZDqKtAQcwI" + "/" + method
     r = requests.post(basic, data = payload)
+    logger.log(logging.WARNING, str(r.text))
     print(r.status_code)
     print(r.text)
     return basic
@@ -32,6 +33,6 @@ def entry():
     for i in range(len(keys)):
         inline['inline_keyboard'].append([{"text": redis.get(keys[i]), "callback_data": keys[i]}])
     outline = json.dumps(inline) 
-    telegram_url_builder("sendMessage", {"chat_id": 957931698, "text": "Утвердить", "reply_markup": outline})
+    telegram_url_builder("sendMessage", {"chat_id": data["message"]["chat"]["id"], "text": "Утвердить", "reply_markup": outline})
     logger.log(logging.WARNING, str(data))
     return {"text": str(data)}
